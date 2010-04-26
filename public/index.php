b5577c72904547ac85c91e6f2c329b1a776276a8
Chris Metcalf <chris@chrismetcalf.net>
2010-04-26 15:04:46 -0700

    Initial import

---
+++ public/index.php
@@ -0,0 +1,83 @@
+<?php
+  require_once("socrata.php");
+
+  $view_uid = array_get("view_uid", $_POST);
+  $data_site = array_get("data_site", $_POST);
+  $response = NULL;
+  if($view_uid != NULL && $data_site != NULL) {
+    // Create a new unauthenticated client
+    $socrata = new Socrata("http://$data_site/api");
+
+    $params = array();
+    $row_ids_only = array_get("row_ids_only", $_POST);
+    if($row_ids_only != NULL && $row_ids_only == "on") {
+      $params["row_ids_only"] = "true";
+      $row_ids_only = true;
+    }
+    if(array_get("max_rows", $_POST) != NULL && array_get("max_rows", $_POST) > 0) {
+      $params["max_rows"] = array_get("max_rows", $_POST);
+    }
+
+    // Request rows from the DC Hospitals view
+    $response = $socrata->get("/views/$view_uid/rows.json", $params);
+  }
+?>
+<html>
+  <head>
+    <title>Socrata API PHP Example</title>
+  </head>
+  <body>
+    <h1>Socrata API PHP Example</h1>
+
+    <?php if($response == NULL) { ?>
+      <form action="index.php" method="post">
+        <label for="data_site">Data Site:</label>
+        <select name="data_site">
+          <option value="data.medicare.gov" selected="true">Data.Medicare.Gov</option>
+          <option value="data.seattle.gov">Data.Seattle.Gov</option>
+          <option value="www.socrata.com">Socrata</option>
+        </select><br/>
+
+        <label for="view_uid">View ID</label>
+        <input type="text" name="view_uid" size="10"/><br/>
+
+        <label for="row_ids_only">Return only row IDs:</label>
+        <input type="checkbox" name="row_ids_only"/><br/>
+
+        <label for="max_rows">Return only # rows:</label>
+        <input type="text" name="max_rows" size="4"/><br/>
+
+        <input type="submit" value="Submit"/>
+      </form>
+    <?php } else { ?>
+      <h2><?= $response["meta"]["view"]["name"] ?></h2>
+      <p><?= $response["meta"]["view"]["description"] ?></p>
+
+      <?# Create a table for our actual data ?>
+      <table border="1">
+        <tr>
+          <?# Print header row ?>
+          <?php foreach($response["meta"]["view"]["columns"] as $column) { ?>
+            <th><?= $column["name"] ?></th>
+          <?php } ?>
+        </tr>
+        <?# Print rows ?>
+        <?php foreach($response["data"] as $row) { ?>
+          <tr>
+            <?php if($row_ids_only == true) { ?>
+              <td><?= $row ?></td>
+            <?php } else { ?>
+              <?php foreach($row as $cell) { ?>
+                <td><?= $cell ?></td>
+              <?php } ?>
+            <?php } ?>
+          </tr>
+        <?php } ?>
+      </table>
+
+      <h3>Raw Response</h3>
+      <pre><?= var_dump($response) ?></pre>
+    <?php } ?>
+  </body>
+</html>
+
---
+++ public/socrata.php
@@ -0,0 +1,70 @@
+<?php
+
+class Socrata {
+  // The base URL for this Socrata API, ex: http://data.medicare.gov/api or http://www.socrata.com/api
+  private $root_url = "";
+
+  // Username and password, used for authenticated requests
+  private $user_name = "";
+  private $password = "";
+
+  // Basic constructor
+  public function __construct($root_url = "", $user_name = "", $password = "") {
+    $this->root_url = $root_url;
+    $this->user_name = $user_name;
+    $this->password = $password;
+    return true;
+  }
+
+  // Convenience function for GET calls
+  public function get($path, $params = array()) {
+
+    // The full URL for this resource is the root + the path
+    $full_url = $this->root_url . $path;
+
+    // Build up our array of parameters
+    $parameters = array();
+    foreach($params as $key => $value) {
+      array_push($parameters, urlencode($key) . "=" . urlencode($value));
+    }
+    if(count($parameters) > 0) {
+      $full_url .= "?" . implode("&", $parameters);
+    }
+
+    // Build up the headers we'll need to pass
+    $headers = array(
+      'Accept: application/json',
+      'Content-type: application/json'
+    );
+
+    // Time for some cURL magic...
+    $handle = curl_init();
+    curl_setopt($handle, CURLOPT_URL, $full_url);
+    curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+
+    // Set up request, and auth, if configured
+    if($this->user_name != "" && $this->password != "") {
+      curl_setopt($handle, CURLOPT_USERPWD, $this->user_name . ":" . $this->password);
+    }
+
+    $response = curl_exec($handle);
+    $code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+    if($code != "200") {
+      echo "Error \"$code\" from server: $response";
+      die();
+    }
+
+    return json_decode($response, true);
+  }
+}
+
+// Convenience functions
+function array_get($needle, $haystack) {
+  return (in_array($needle, array_keys($haystack)) ? $haystack[$needle] : NULL);
+}
+
+function pre_dump($var) {
+  echo "<pre>" . print_r($var) . "</pre>";
+}
+?>
